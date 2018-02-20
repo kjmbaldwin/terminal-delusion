bdbec68d8b2ea6aad900a63bb498a72469ef03ff
Kyle Baldwin <kylejmbaldwin@gmail.com>
2018-02-19 21:54:20 -0500

    enemy seeder is now working

---
+++ seeders/20180220021902-enemy-seeds.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return queryInterface.bulkInsert('enemies', [{
+      name: 'Ogre',
+      level: 1,
+      max_hp: 10,
+      base_attack: 5,
+      known_skills: "attack",
+      createdAt : new Date(),
+      updatedAt : new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('enemies', null, {});
+  }
+};
